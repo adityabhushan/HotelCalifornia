12dbc2137e539a98889da8930cb9ec0bfaee98c9
Aditya <aditya@rangde.org>
2017-01-31 16:44:55 +0530

    Add port process.env for PORT

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const http = require('http').Server(app)
 const path = require('path')
-const PORT = 3000
+const PORT = 3000 || process.env.PORT
 let usernames = []
 
 app.use('/',express.static(path.join(__dirname, 'public')))
