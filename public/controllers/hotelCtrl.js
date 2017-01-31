b5f5fd1a8f45118fd42bc13eb7daa25baec2c9cf
Aditya <aditya@rangde.org>
2017-01-31 17:17:13 +0530

    Add room to localstorage

--- public/controllers/hotelCtrl.js
+++ public/controllers/hotelCtrl.js
@@ -3,10 +3,16 @@
 
     .controller('hotelCtrl', ['$scope', function($scope) {
 
-        $scope.rooms = [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }]
+        if(!localStorage.getItem('totalRooms')) {
+        	$scope.rooms = [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }]            
+            localStorage.setItem('totalRooms', JSON.stringify($scope.rooms))
+        } else {
+            $scope.rooms = JSON.parse(localStorage.getItem('totalRooms'))
+        }
 
         $scope.addRoom = function() {
             $scope.rooms.push({ id: $scope.rooms.length + 1 })
+            localStorage.setItem('totalRooms', JSON.stringify($scope.rooms))
         }
     }])
 })()
