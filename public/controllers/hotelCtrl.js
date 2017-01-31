c2e08c96839d6af937fd281b56e37ba15e7da80d
Aditya <aditya@rangde.org>
2017-01-31 12:18:00 +0530

    change defualt no of rooms to 4

--- public/controllers/hotelCtrl.js
+++ public/controllers/hotelCtrl.js
@@ -2,8 +2,8 @@
     angular.module('room')
 
     .controller('hotelCtrl', ['$scope', function($scope) {
-    	
-        $scope.rooms = [{ id: 1 }, { id: 2 }, { id: 3 }]
+
+        $scope.rooms = [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }]
 
         $scope.addRoom = function() {
             $scope.rooms.push({ id: $scope.rooms.length + 1 })
